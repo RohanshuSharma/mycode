b531124d2b18bd7e4fccc600784a11457b7a0c66
Sean Allen <allen.sean.t@gmail.com>
2018-09-01 09:18:46 -0700

    Initial Commit

---
+++ MapKit-Directions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKit-Directions
+//
+//  Created by Sean Allen on 9/1/18.
+//  Copyright © 2018 Sean Allen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
